0f7b7fe764f007b4077bd146797a0e2e2b35eeff
JJennings-Glidewell <jason.jennings@glidewelldental.com>
2017-08-24 11:43:36 -0700

    here ya go

---
+++ contact-page.php
@@ -0,0 +1,27 @@
+
+<?php
+/*
+Template Name: GDR ~ Contact Us
+Description: This part is optional, but helpful for describing the Post Template
+*/
+?>
+
+<?php
+
+get_header();
+
+?>
+<div class="container margin-top">
+
+  <div class="row">
+    <div class="col-sm-6">
+      
+    </div>
+    <div class="col-sm-6">
+      <script type="text/javascript" src="https://form.jotform.us/jsform/72115228732148"></script>
+    </div>
+  </div>
+  
+</div>
+ 
+<?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -1,10 +1,74 @@
-			<!-- Start: footer -->
-			<footer></footer>
-			<!-- End: footer -->
+<footer class="resourcesfooter">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+        <h3>Contact Information</h3>
+        <ul class="contact-info">
+				  <li>
+				    <p><b><i class="fa fa-phone" aria-hidden="true"></i> PHONE:</b><br>
+				      <a href="tel:1-888-303-3975">888-303-3975 USA</a><br>
+				      <a href="tel:1-888-278-0414">888-278-0414 Canada</a></p>
+				  </li>
+				  <li>
+				    <p><b><i class="fa fa-envelope-o" aria-hidden="true"></i> EMAIL:</b><br>
+				      <a href="mailto:mail@glidewelldirect.com">Click to Send Email</a> </p>
+				  </li>
+				  <li>
+				    <p><b><i class="fa fa-clock-o" aria-hidden="true"></i> WORKING DAYS/HOURS:</b><br>
+				      Monday – Friday <br>
+				      6 a.m. – 5 p.m. PST </p>
+				  </li>
+				  <li>
+				    <p><b><i class="fa fa-file-o" aria-hidden="true"></i> Taxpayer ID#:</b><br>
+				      #45-2822065 (<a href="https://cdn.shopify.com/s/files/1/1616/9759/files/GD-w-9.pdf" target="_blank">W-9 PDF</a>)</p>
+				  </li>
+				</ul>
+    	</div>
+			<div class="col-sm-2">
+				<h3>About</h3>
+				<ul class="footer-list">
+        	<li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/pages/about-us"> Company</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/pages/catalogs"> Catalogs</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/pages/shipping"> Shipping</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/pages/policies"> Policies</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/pages/warranty"> Warranty</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/pages/trademarks"> Trademarks</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/apps/help-center"> FAQs</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/account"> Account</a></li>
+        </ul>
+			</div>
+  		<div class="col-sm-3">
+				<h3>Glidewell Dental Education</h3>
+				<ul class="footer-list">
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="http://glidewelldental.com/education/chairside-dental-magazine/"> Chairside<sup>®</sup> magazine</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="http://glidewelldental.com/education/chairside-live/"> Chairside Live</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="http://glidewelldental.com/education/on-demand-ce-courses/"> On-Demand CE Courses</a></li>
+          <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="/collections/education"> Hands-on Course</a></li>
+        </ul>
+			</div>
+			<div class="col-sm-4">
+				<h3>Subscribing Has Its Perks!</h3>
+				<ul class="footer-list">
+				  <li><i class="fa fa-check" aria-hidden="true"></i> Offers on products you may already use</li>
+				  <li><i class="fa fa-check" aria-hidden="true"></i> Introductory offers on new products</li>
+				  <li><i class="fa fa-check" aria-hidden="true"></i> Educational materials</li>
+				</ul>
+				<a class="btn foot-btn" href="https://beta.glidewelldirect.com/pages/sign-up">Subscribe Now</a>
+			</div> 
+		</div>
+	</div>
+</footer>
+<div class="footer-bottom">   
+  <div class="container">
+ 		<address>
+      © Glidewell Direct 
+    </address>    
+  </div>     
+</div>
+<!-- End: footer -->
 			<!-- Start:  -->
 			<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
-
